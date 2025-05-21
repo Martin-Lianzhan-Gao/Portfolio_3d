9188f24db0b7644fb7596ae99f072f1ca26edf9d
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-05-21 23:06:00 +1000

    feat(deps): install lucide-react for icons usage

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => { 
+    return (
+        <div className="w-full">
+            <ul className="flex flex-row justify-between py-4 px-4 max-w-">
+                <li>HOME</li>
+                <li>EXPERIENCE</li>
+                <li>PROJECT</li>
+                <li>EDUCATION</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
+
