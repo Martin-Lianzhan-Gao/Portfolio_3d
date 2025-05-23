170749676a0d9b5daa14c905264a4c651e7d49da
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-05-24 04:42:00 +1000

    feat(header): add scroll animation for desktop screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { Menu, X } from "lucide-react";
 import { cn } from "@/utils/cn";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect} from "react";
+import { motion, AnimatePresence, useScroll } from "framer-motion";
+import useIsMobile from "@/hooks/useIsMobile";
+import useScrollDirection from "@/hooks/useScrollDirection";
 
 const AnimateListItem = ({ text, index} : {text: string, index: number}) => { 
     return (
@@ -22,21 +24,50 @@ const AnimateListItem = ({ text, index} : {text: string, index: number}) => {
     )
 } 
 
-const Header = () => { 
-    
+const Header = () => {
+    // get screen type
+    const isMobile = useIsMobile();
+    // get vertical scroll position
+    const scrollDirection = useScrollDirection();
+    // control the menu / header open state in mobile devices
     const [isOpen, setIsOpen] = useState(false);
-
+    // control the header visibility in desktop devices
+    const [isScrollUp, setIsScrollUp] = useState(true);
+    // state represents whether scolling
+    const [isScrolling, setIsScrolling] = useState(false);
+    // header / menu items
     const itemTexts = ['Home', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'];
+    // determine control state of the header based on the screen type
+    const showHeader = isMobile ? isOpen : isScrollUp;
+
+    useEffect(() => {
+        
+        if (scrollDirection === null) {
+            setIsScrolling(false);
+        } else { 
+            setIsScrolling(true);
+        }
+
+        if (isScrolling) { 
+            if (scrollDirection === "up") {
+                setIsScrollUp(true);
+            }
+            if (scrollDirection === "down") {
+                setIsScrollUp(false);
+            }
+        }
+        
+    }, [scrollDirection, isScrolling]);
 
     return (
-        <div className="fixed w-full">
+        <div className="fixed w-full backdrop-blur-xl">
             <div>
                 <button className="ml-4 my-2 flex flex-row px-3 py-2 rounded-3xl md:hidden" onClick={() => setIsOpen(!isOpen)}>
                     { isOpen ? <X /> : <Menu /> }
                     Menu
                 </button>
                 <AnimatePresence>
-                    {isOpen && (
+                    {showHeader && (
                         <motion.ul className={cn("flex my-2 mx-4 flex-col items-center h-1/2", "md:flex md:flex-row md:justify-between md:my-4")}>
                             {Array.from({ length: 6 }).map((_, index) => (
                                 <AnimateListItem key={index} index={index} text={itemTexts[index]} />
@@ -49,5 +80,5 @@ const Header = () => {
     )
 }
 
-export default Header;
 
+export default Header;
