0a32677e3c0d15a592144091802daa00c9e54cef
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-05-29 03:39:07 +1000

    delete(header): delete useless imports to pass eslint safety check

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Menu, X } from "lucide-react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import useIsMobile from "@/hooks/useIsMobile";
 import useScrollDirection from "@/hooks/useScrollDirection";
