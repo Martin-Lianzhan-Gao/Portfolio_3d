181f23e089a0ca80cee9d5e240a4d0b3b17c89de
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-05-29 20:49:04 +1000

    feat(header): add indicator with relevant on-selected animation on selected header item

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { Menu, X } from "lucide-react";
-import { useState, useEffect} from "react";
+import { useState, useEffect, Dispatch, SetStateAction} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import useIsMobile from "@/hooks/useIsMobile";
 import useScrollDirection from "@/hooks/useScrollDirection";
+import { cn } from "@/utils/cn";
 
-const AnimateListItem = ({ text, index }: { text: string, index: number }) => {
+const AnimateListItem = ({ text, index, isSelected, textSetter }: { text: string, index: number, isSelected: boolean, textSetter: Dispatch<SetStateAction<string>> }) => {
+
     return (
         <motion.li
             initial={{ y: -20, opacity: 0 }}
@@ -16,9 +18,26 @@ const AnimateListItem = ({ text, index }: { text: string, index: number }) => {
                 duration: 0.3,
                 ease: "easeInOut",
             }}
-            className="py-2 md:py-0"
+            className={cn("py-2 text-gray-300 hover:text-gray-100 md:py-0", isSelected ? "text-gray-100" : "text-gray-300" , "cursor-pointer ")}
+            onClick={() => textSetter(text)}
         >
             {text}
+            <div className="w-full flex justify-center h-[2px]">
+                <AnimatePresence>
+                    {isSelected && (<motion.div
+                        className="w-[12px] h-[2px] rounded-2xl bg-gray-100"
+                        initial={{ width: 0, opacity: 0 }}
+                        animate={{ width: '12px', opacity: 1 }}
+                        exit={{ width: 0, opacity: 0 }}
+                        transition={{
+                            delay: 0.03 + 0.01,
+                            duration: 0.3,
+                            ease: "easeInOut",
+                        }}
+                    />)}
+                </AnimatePresence>
+
+            </div>
         </motion.li>
     )
 }
@@ -38,6 +57,8 @@ const Header = () => {
     const itemTexts = ['Home', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'];
     // determine control state of the header based on the screen type
     const showHeader = isMobile ? isOpen : isScrollUp;
+
+    const [selectedText, setSelectedText] = useState(itemTexts[0]);
 
     // useEffect to handle the scroll direction and update the header visibility
     useEffect(() => {
@@ -60,9 +81,9 @@ const Header = () => {
     }, [scrollDirection, isScrolling]);
 
     return (
-        <div className="fixed z-10 text-gray-300 w-full">
+        <div className="fixed z-10 w-full">
             <div className="backdrop-blur-xl py-2 md:hidden">
-                <button className="ml-4 flex flex-row px-3 py-2 rounded-3xl md:hidden" onClick={() => setIsOpen(!isOpen)}>
+                <button className="ml-4 flex flex-row px-3 py-2 rounded-3xl text-gray-100 md:hidden" onClick={() => setIsOpen(!isOpen)}>
                     {isOpen ? <X /> : <Menu />}
                     Menu
                 </button>
@@ -86,7 +107,13 @@ const Header = () => {
                                 className="flex flex-col items-center md:flex md:flex-row md:justify-around"
                             >
                                 {Array.from({ length: 6 }).map((_, index) => (
-                                    <AnimateListItem key={index} index={index} text={itemTexts[index]} />
+                                    <AnimateListItem
+                                        key={index}
+                                        index={index}
+                                        text={itemTexts[index]}
+                                        isSelected={selectedText === itemTexts[index]}
+                                        textSetter={setSelectedText}
+                                    />
                                 ))}
                             </motion.ul>
                         </div>
