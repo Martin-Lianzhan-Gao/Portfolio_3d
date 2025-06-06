8fbc068594645329c1da637eed1488961cba2b31
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-06 16:46:32 +1000

    feat(cities): add and export cities data

---
+++ src/data/cities.ts
@@ -0,0 +1,20 @@
+
+export const zhengzhou = {
+    // Zhengzhou, China
+    longitude: 113.6604,
+    latitude: 34.7538,
+    
+}
+
+export const brisbane = {
+    // Brisbane, Australia
+    longitude: 153.021072,
+    latitude: -27.470125,
+    
+}
+
+export const sydney = {
+    longitude: 151.2099,
+    latitude: -33.8688
+}
+
