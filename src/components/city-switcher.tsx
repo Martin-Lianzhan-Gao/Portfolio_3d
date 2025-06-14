a15dd4cbde6abc1f1c674fae712de93bb8cbf123
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-14 11:44:25 +1000

    UI(city-switcher): refactor the UI to make it more intuitive

---
+++ src/components/city-switcher.tsx
@@ -0,0 +1,43 @@
+import { memo, forwardRef } from 'react';
+import { cn } from '@/utils/cn';
+import { Dispatch, SetStateAction } from 'react';
+import { zhengzhou, brisbane } from '@/data/cities';
+import { motion } from 'framer-motion';
+
+interface CitySwitcherProps {
+    onSelectCity: ({ longitude, latitude }: { longitude: number, latitude: number }) => void;
+    isCurrentLocation: boolean,
+    setIsCurrentLocation: Dispatch<SetStateAction<boolean>>,
+}
+
+const CitySwitcher = ({ onSelectCity, isCurrentLocation, setIsCurrentLocation }: CitySwitcherProps) => {
+
+    return (
+
+        <motion.div
+            className='mt-4 text-sm text-gray-400 flex flex-row md:text-xl 2xl:text-2xl'
+        >
+            <motion.div
+                onClick={() => {
+                    setIsCurrentLocation(true);
+                    onSelectCity({ longitude: brisbane.longitude, latitude: brisbane.latitude });
+                }}
+                className={cn(isCurrentLocation ? "text-gray-200 bg-gray-700/40 border border-white/20" : "cursor-pointer hover:text-gray-200 hover:bg-gray-700/40 active:bg-gray-700/40 active:text-gray-200 transition-colors duration-200", "px-4 py-2 rounded-2xl  backdrop-blur-2xl mr-1")}
+            >
+                Location
+            </motion.div>
+            <motion.div
+                onClick={() => {
+                    setIsCurrentLocation(false);
+                    onSelectCity({ longitude: zhengzhou.longitude, latitude: zhengzhou.latitude });
+                }}
+                className={cn(!isCurrentLocation ? "text-gray-200 bg-gray-700/40 border border-white/20" : "cursor-pointer hover:text-gray-200 hover:bg-gray-700/40 active:bg-gray-700/40 active:text-gray-200 transition-colors duration-200", "px-4 py-2 rounded-2xl  backdrop-blur-2xl ml-1")}
+            >
+                Hometown
+            </motion.div>
+        </motion.div>
+
+    )
+};
+
+export default memo(CitySwitcher);
