bb9c689eed99d1837a1bc128122b6df7cb7a68c9
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-15 18:41:53 +1000

    Feat(navigation button): Add a global navigation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Intro from "@/components/intro";
+import NavigationButton from "@/components/navigation-button";
 
 
 export default function Home() {
     return (
         <div className="overflow-x-hidden">
             <Intro />
-            
             <div className="flex w-full h-screen flex-col items-center justify-center p-24">
                 First Page
             </div>
             <div className="flex h-screen flex-col items-center justify-center p-24">
                 Second Page
+            </div>
+            <div className="flex w-full flex-row justify-center fixed z-12 bottom-8 text-black">
+                <NavigationButton text="Next" isLastPart={false} />
             </div>
         </div>
         
