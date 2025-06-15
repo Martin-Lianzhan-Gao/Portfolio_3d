ee3620c2d77f2b6fc249f81cfff24240df36b417
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-15 18:40:12 +1000

    UI: adjust font and icon size to integrate style

--- src/components/navigation-button.tsx
+++ src/components/navigation-button.tsx
@@ -1,14 +1,19 @@
-import { motion } from "framer-motion"
-import { Compass } from "lucide-react"
+import { ArrowUp, ArrowDown } from "lucide-react"
 
-const NavigationButton = ({ text }: { text: string }) => {
+type NavigationButtonProps = {
+    text: string;
+    isLastPart?: boolean;
+    setIsLastPart?: (value: boolean) => void;
+ }
+
+const NavigationButton = ({ text, isLastPart}: NavigationButtonProps) => {
     return (
-        <motion.button
-            className="flex flex-row items-center justify-center px-3 py-2 mt-4 border border-white/20 bg-gray-800/30 rounded-3xl backdrop-blur-2xl hover:bg-gray-700/40 active:bg-gray-700/40 transition-colors duration-200 text-sm"
+        <button
+            className="flex flex-row items-center justify-center px-4 py-2 mt-4 border border-white/20 bg-gray-800/30 rounded-3xl backdrop-blur-2xl hover:bg-gray-700/40 active:bg-gray-700/40 transition-colors duration-200 text-md font-roboto-mono"
         >
-            <Compass />
+            {isLastPart ? <ArrowUp size={18} /> : <ArrowDown size={18} />}
             <span className="ml-2">{text}</span>
-        </motion.button>
+        </button>
     )
 }
 
