06146b557d50fe8cfe8649bcf2b5d8c801bece5b
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-15 19:34:00 +1000

    Feat(jotai): add animation to the navigation button controlled by jotai atom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import Intro from "@/components/intro";
 import NavigationButton from "@/components/navigation-button";
-
+import { useAtomValue } from "jotai";
+import { isTransitioningAtom } from "@/atoms/atoms-store";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
+    const isTransitioning = useAtomValue(isTransitioningAtom);
+
     return (
         <div className="overflow-x-hidden">
             <Intro />
@@ -12,10 +17,19 @@ export default function Home() {
             <div className="flex h-screen flex-col items-center justify-center p-24">
                 Second Page
             </div>
-            <div className="flex w-full flex-row justify-center fixed z-12 bottom-8 text-black">
-                <NavigationButton text="Next" isLastPart={false} />
-            </div>
+            <AnimatePresence>
+                {!isTransitioning && (
+                    <motion.div
+                        className="flex w-full flex-row justify-center fixed z-12 bottom-8 text-black"
+                        initial={{ y: 100, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: 100, opacity: 0 }}
+                        transition={{ duration: 0.8, ease: "easeInOut" }}
+                    >
+                        <NavigationButton text="Next" isLastPart={false} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
-        
     );
 }
