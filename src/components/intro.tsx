a9d68f36dfc40cc78be45ef15cbe680b9ab385a9
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-17 20:56:41 +1000

    Feat(animation): optimize the enter and exit animation of information card by using spring effect

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -91,30 +91,59 @@ const Intro = () => {
 
     }, [isMobile, currentCity, mapLoaded]);
 
-    // define the animations for the container
-    const containerVariants: Variants = {
-        // exit animation
+    const itemVariants: Variants = {
+        initial: { x: -100, opacity: 0 },
+        animate: { 
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: "spring",
+                stiffness: 100,
+                damping: 20,
+            }
+        },
         exit: {
             x: -100,
             opacity: 0,
             transition: {
-                duration: 0.8,
-                ease: "easeInOut"
+                type: "spring",
+                stiffness: 70,
+                damping: 20,
+                mass: 0.6,
             }
+         }
+    }
+
+    // define the animations for the container
+    const containerVariants: Variants = {
+        // initial state
+        initial: {
+            x: -100,
+            opacity: 0,
         },
         // enter animation
         animate: {
             x: 0,
             opacity: 1,
             transition: {
-                duration: 0.8,
-                ease: "easeInOut",
+                type: "spring",
+                stiffness: 70,
+                damping: 20,
+                mass: 0.6,
+                staggerChildren: 0.15, // stagger children animations by 0.15 seconds
             },
         },
-        // initial state
-        initial: {
+        // exit animation
+        exit: {
             x: -100,
             opacity: 0,
+            transition: {
+                type: "spring",
+                stiffness: 70,
+                damping: 20,
+                mass: 0.6,
+                staggerChildren: 0.15, // stagger children animations by 0.15 seconds
+            },
         }
     }
 
@@ -126,38 +155,61 @@ const Intro = () => {
             <div className="w-full h-full">
                 <WorldMap ref={mapRef} setMapLoaded={setMapLoaded} />
             </div>
-            <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-auto flex flex-col justify-end items-center border border-white/20 z-2 md:w-1/2 md:justify-center">
+            <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-auto flex flex-col justify-end items-center z-2 md:w-1/2 md:justify-center">
                 <AnimatePresence>
-                    {!isTransitioning && <motion.div
-                        className="backdrop-blur-xl border bg-black-800/30 border-white/20 rounded-3xl p-4 ml-4 mr-4  h-auto w-auto mb-30 md:ml-10 md:mr-10 md:max-w-[728px] md:mb-0"
+                    {!isTransitioning && (<motion.div
+                        className="backdrop-blur-sm border bg-black-800/30 border-white/20 rounded-3xl p-4 ml-4 mr-4  h-auto w-auto mb-30 md:ml-10 md:mr-10 md:max-w-[728px] md:mb-0"
                         variants={containerVariants}
                         initial="initial"
                         animate="animate"
                         exit="exit"
                         ref={cardRef}
                     >
-                        <motion.h1
-                            className="font-bold text-3xl md:text-4xl 2xl:text-5xl"
-                        >
-                            HI, I&apos;M LIANZHAN GAO (MARTIN)
-                        </motion.h1>
-                        <motion.h2
-                            className="font-medium mt-1 text-xl md:text-2xl 2xl:text-3xl"
-                        >
-                            - 1/1 Developer<br />
-                            - 1/2 Designer
-                        </motion.h2>
-                        <motion.p
-                            className="mt-2 text-sm md:text-xl 2xl:text-2xl"
-                        >
-                            A motivated and results-driven Computer Science graduate with over a year of hands-on experience in full-stack development and cloud development. And also a fast learner with a practical and exploratory spirit.
-                        </motion.p>
-                        <CitySwitcher
-                            onSelectCity={onChangeCity}
-                            isCurrentLocation={isCurrentLocation}
-                            setIsCurrentLocation={setIsCurrentLocation}
-                        />
-                    </motion.div>}
+                        <AnimatePresence>
+                            <motion.h1
+                                key={1}
+                                className="font-bold text-3xl md:text-4xl 2xl:text-5xl"
+                                variants={itemVariants}
+                            >
+                                HI, I&apos;M LIANZHAN GAO (MARTIN)
+                            </motion.h1>
+                            <motion.h2
+                                key={2}
+                                className="font-medium mt-1 text-xl md:text-2xl md:mt-2 2xl:text-3xl 2xl:mt-3"
+                                variants={itemVariants}
+                            >
+                                <motion.span
+                                    key={3}
+                                    variants={itemVariants}
+                                >
+                                    - 1/1 Developer 🧑🏻‍💻
+                                </motion.span><br />
+                                <motion.span
+                                    key={4}
+                                    variants={itemVariants}
+                                >
+                                    - 1/2 Designer 🎨
+                                </motion.span>
+                            </motion.h2>
+                            <motion.p
+                                key={5}
+                                className="mt-2 text-sm md:text-xl md:mt-4 2xl:text-2xl 2xl:mt-5"
+                                variants={itemVariants}
+                            >
+                                A motivated and results-driven Computer Science graduate with over a year of hands-on experience in full-stack development and cloud development. And also a fast learner with a practical and exploratory spirit 🚀.
+                            </motion.p>
+                            <motion.div
+                                key={6}
+                                variants={itemVariants}
+                            >
+                                <CitySwitcher
+                                    onSelectCity={onChangeCity}
+                                    isCurrentLocation={isCurrentLocation}
+                                    setIsCurrentLocation={setIsCurrentLocation}
+                                />
+                            </motion.div>
+                        </AnimatePresence>
+                    </motion.div>)}
                 </AnimatePresence>
             </div>
         </div>
