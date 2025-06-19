1502b766c3a35851e480af37b38a218cfadd2db8
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-06-19 17:53:37 +1000

    UI: refactor the UI and open/close animation onboth desktop devices and mobile devices

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,44 +1,33 @@
 'use client'
 import { Menu, X } from "lucide-react";
-import { useState, useEffect, Dispatch, SetStateAction} from "react";
+import { useState, useEffect, useRef, Dispatch, SetStateAction} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import useIsMobile from "@/hooks/use-is-mobile";
 import useScrollDirection from "@/hooks/use-scroll-direction";
 import { cn } from "@/utils/cn";
 
-const AnimateListItem = ({ text, index, isSelected, textSetter }: { text: string, index: number, isSelected: boolean, textSetter: Dispatch<SetStateAction<string>> }) => {
+const AnimateListItem = ({ text, isSelected, textSetter }: { text: string, index: number, isSelected: boolean, textSetter: Dispatch<SetStateAction<string>> }) => {
 
     return (
-        <motion.li
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -20, opacity: 0 }}
-            transition={{
-                delay: index * 0.03,
-                duration: 0.3,
-                ease: "easeInOut",
-            }}
-            className={cn("py-4 text-gray-300 hover:text-gray-100 md:py-0", isSelected ? "text-gray-100" : "text-gray-300" , "cursor-pointer ")}
+        <motion.div
+            className={cn("text-gray-300 hover:text-gray-100 py-6 md:py-0", isSelected ? "text-gray-100" : "text-gray-300", "cursor-pointer ")}
             onClick={() => textSetter(text)}
         >
             {text}
-            <div className="w-full flex justify-center h-[2px]">
-                <AnimatePresence>
-                    {isSelected && (<motion.div
-                        className="w-[12px] h-[2px] rounded-2xl bg-gray-200"
-                        initial={{ width: 0, opacity: 0 }}
-                        animate={{ width: '12px', opacity: 1 }}
-                        exit={{ width: 0, opacity: 0 }}
-                        transition={{
-                            delay: 0.03 + 0.01,
-                            duration: 0.3,
-                            ease: "easeInOut",
-                        }}
-                    />)}
-                </AnimatePresence>
-
-            </div>
-        </motion.li>
+            <motion.div className="w-full flex justify-center h-[2px]">
+                {isSelected && (<motion.div
+                    className="w-[12px] h-[2px] rounded-2xl bg-gray-200"
+                    initial={{ width: 0, opacity: 0 }}
+                    animate={{ width: '12px', opacity: 1 }}
+                    exit={{ width: 0, opacity: 0 }}
+                    transition={{
+                        delay: 0.03 + 0.01,
+                        duration: 0.3,
+                        ease: "easeInOut",
+                    }}
+                />)}
+            </motion.div>
+        </motion.div>
     )
 }
 
@@ -60,6 +49,14 @@ const Header = () => {
 
     const [selectedText, setSelectedText] = useState(itemTexts[0]);
 
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const handleClickOutside = (event: TouchEvent) => { 
+        if (isMobile && menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setIsOpen(false);
+        }
+    }
+
     // useEffect to handle the scroll direction and update the header visibility
     useEffect(() => {
 
@@ -80,10 +77,23 @@ const Header = () => {
 
     }, [scrollDirection, isScrolling]);
 
+    useEffect(() => {
+
+        handleClickOutside;
+
+        // Add event listener for click events
+        document.addEventListener('touchstart', handleClickOutside);
+
+        return () => {
+
+        }
+
+    }, [isMobile, isOpen]);
+
     return (
-        <div className="fixed z-10 w-full backdrop-blur-sm font-roboto-mono">
-            <div className="py-2 md:hidden">
-                <button className="ml-4 flex flex-row px-3 py-2 rounded-3xl text-gray-100 md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        <div className={cn("fixed z-10 w-full font-roboto-mono flex flex-col items-center", isOpen && isMobile ? "h-screen" : "h-auto")}>
+            <div className="py-2 backdrop-blur-sm w-full md:hidden">
+                <button className=" ml-4 flex flex-row px-3 py-2 text-gray-100 md:hidden" onClick={() => setIsOpen(!isOpen)}>
                     {isOpen ? <X /> : <Menu />}
                     Menu
                 </button>
@@ -91,32 +101,23 @@ const Header = () => {
 
             <AnimatePresence>
                 {showHeader && (
-                    <motion.div className="w-full flex flex-row justify-center"
-                        initial={{ height: 0, opacity: 0 }}
-                        animate={{ height: 'auto', opacity: 1 }}
-                        exit={{ height: 0, opacity: 0 }}
-                        transition={{
-                            delay: 5 * 0.03,
-                            duration: 0.3,
-                            ease: "easeInOut",
-                        }}
+                    <motion.div
+                        ref={menuRef}
+                        className="border border-white/20 rounded-2xl backdrop-blur-sm w-[calc(100%-16px)] h-1/2 my-4 ml-4 mr-4 flex flex-col justify-center items-center md:flex-row md:justify-around md:h-auto md:w-full md:mr-0 md:ml-0 md:my-0 md:py-4 md:rounded-none md:border-0"
                         layout
+                        initial={isMobile ? { opacity: 0, y: -100, scale: 0.4 } : {opacity: 0, y: -20}}
+                        animate={isMobile ? { opacity: 1, y: 0, scale: 1 } : { opacity: 1, y: 0 }}
+                        exit={isMobile ? { opacity: 0, y: -100, scale: 0.4 } : { opacity: 0, y: -20 }}
                     >
-                        <div className="w-full py-2 md:py-4 md:max-w-7xl md:min-w-3xl md:px-4">
-                            <motion.ul
-                                className="flex flex-col items-center md:flex md:flex-row md:justify-around"
-                            >
-                                {Array.from({ length: 6 }).map((_, index) => (
-                                    <AnimateListItem
-                                        key={index}
-                                        index={index}
-                                        text={itemTexts[index]}
-                                        isSelected={selectedText === itemTexts[index]}
-                                        textSetter={setSelectedText}
-                                    />
-                                ))}
-                            </motion.ul>
-                        </div>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                            <AnimateListItem
+                                key={index}
+                                index={index}
+                                text={itemTexts[index]}
+                                isSelected={selectedText === itemTexts[index]}
+                                textSetter={setSelectedText}
+                            />
+                        ))}
                     </motion.div>
                 )}
             </AnimatePresence>
